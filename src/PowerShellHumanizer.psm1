752bc10ef0182daf3caef104731c3e27c416e7a5
Kris Borowinski <kris.borowinski@outlook.com>
2024-07-07 02:08:51 +0200

    Update Humanizer package to latest version; Add support for PowerShell 5.1; Try ISO language name as last resort when adding resources

--- src/PowerShellHumanizer.psm1
+++ src/PowerShellHumanizer.psm1
@@ -1,9 +1,25 @@
 # Load  dlls
+if ($PSVersionTable.PSVersion.Major -lt 6) {
+    Add-Type -Path "$PSScriptRoot/lib/System.Buffers.4.0.2.0.dll"
+    Add-Type -Path "$PSScriptRoot/lib/System.Runtime.CompilerServices.Unsafe.4.0.4.1.dll"
+}
 Add-Type -Path "$PSScriptRoot/lib/Humanizer.dll"
 
 if ($PSCulture -ne 'en-US') {
-    if (Test-Path "$PSScriptRoot/lib/$PSCulture") {
-        Add-Type -Path "$PSScriptRoot/lib/$PSCulture/Humanizer.resources.dll"
+    $resourcePath = foreach (
+        $culture in
+            $PSCulture,
+            [Globalization.CultureInfo]::CurrentCulture.TwoLetterISOLanguageName
+    ) {
+        $pathCandidate = "$PSScriptRoot/lib/$culture/Humanizer.resources.dll"
+        if (Test-Path $pathCandidate) {
+            $pathCandidate
+            break
+        }
+    }
+
+    if ($resourcePath) {
+        Add-Type -Path $resourcePath
     } else {
         Write-Warning "Humanizer doesn't currently support '$PSCulture'."
     }
